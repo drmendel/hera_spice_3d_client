6d218564035b577f2b517682799ed0fd58b57600
drmendel <dobondireiszmendel@gmail.com>
2025-03-29 17:49:03 +0100

    Add requestPerSec value

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 // ###################### GLOBAL VARIABLES ######################
 
 export let websocketUrl;
+export let requestPerSec = 10;     // req/sec
 export let lightColor = 'rgb(175,175,175)';
 export let darkColor = 'rgb(128,128,128)';
 
