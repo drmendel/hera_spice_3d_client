788b117f5a612e866ec779926fd84f9c2e125024
drmendel <dobondireiszmendel@gmail.com>
2025-03-31 12:10:25 +0200

    Set predefined values to const

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
 // ###################### GLOBAL VARIABLES ######################
 
 export let websocketUrl;
-export let requestPerSec = 10;     // req/sec
-export let lightColor = 'rgb(175,175,175)';
-export let darkColor = 'rgb(128,128,128)';
+export const requestPerSec = 10;     // req/sec
+export const lightColor = 'rgb(175,175,175)';
+export const darkColor = 'rgb(128,128,128)';
+export const minDate = new Date("2024-08-01T00:00:00.000"); // Minimum allowed date
+export const maxDate = new Date("2029-01-01T00:00:00.000"); // Maximum allowed date
 
 // ###################### FUNCTION ######################
 
