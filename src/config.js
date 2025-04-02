3b300a07baff85eca416ab25648bc67afa4f8a11
drmendel <dobondireiszmendel@gmail.com>
2025-04-02 18:30:11 +0200

    Add canvasName to config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 
 export let websocketUrl;
 export const requestPerSec = 10;     // req/sec
+export const canvasName = 'threeCanvas';
 export const lightColor = 'rgb(175,175,175)';
 export const darkColor = 'rgb(128,128,128)';
 export const minDate = new Date("2024-08-01T00:00:00.000"); // Minimum allowed date
