93e69b176b287cebbaafaba44a365398243cfb87
drmendel <dobondireiszmendel@gmail.com>
2025-04-29 23:32:21 +0200

    Adjust sequence of operations in main logic

--- src/main.js
+++ src/main.js
@@ -4,16 +4,18 @@ import * as anim from './animation';
 import * as conf from './config';
 import * as ws from './websocket';
 
-ws.openWebSocket().catch(async () => {
-    ctrl.simulationRunningStore(false);
-    ctrl.setSimulationTime(String("2025-03-12T09:27:00.000"));
-    ctrl.updatePlaceholder();
-    alert("Server unreachable. Attempting reconnection...");
-    ws.wsReconnection();
+ws.waitForOpen().then(() => {
+    ctrl.setParamsFromURL();
 });
 
-anim.loadThreeJSEngine().then( async () => {
+anim.loadThreeJSEngine().then(() => {
     ctrl.setup();
     anim.gsapCamera();
     anim.animate();
-});
+}).then(
+    ws.openWebSocket().catch(async () => {
+        ctrl.simulationRunningStore(false);
+        ctrl.setSimulationTime(String("2025-03-12T09:27:00.000"));
+        ctrl.updatePlaceholder();
+    }
+));
