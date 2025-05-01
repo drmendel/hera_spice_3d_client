29af020ae6d63345e4e3dad37f77bdbdbffa22b7
drmendel <dobondireiszmendel@gmail.com>
2025-05-01 20:52:43 +0200

    Fix decrement button behaviour

--- src/controls.js
+++ src/controls.js
@@ -295,11 +295,12 @@ async function crementSpeed(increment) {
   psi.value = Math.min(10, Math.max(1, Number(psi.value) + (increment ? 1 : -1)));
 
   if(!increment) {
+    const simulationState = simulationRunning;
     simulationRunning = false;
     await waitForMessages(() => data.instantaneousTelemetryData.requestedSize, () => data.lightTimeAdjustedTelemetryData.requestedSize);
     data.instantaneousTelemetryData.reset();
     data.lightTimeAdjustedTelemetryData.reset();
-    simulationRunning = true;
+    simulationRunning = simulationState;
   }
 
   // Update speed and timing references
