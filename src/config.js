c2a6401b3d56fc4572dc8f7c71ff8b9ff8a42d53
drmendel <dobondireiszmendel@gmail.com>
2025-05-19 22:21:00 +0200

    Refactor config.js

--- src/config.js
+++ src/config.js
@@ -1,12 +1,20 @@
-// ###################### GLOBAL VARIABLES ######################
+// ─────────────────────────────────────────────
+// WebSocketUrl Helpers
+// ─────────────────────────────────────────────
 
 const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-const host = window.location.host; // e.g., mywebsite.com
+const host = window.location.host;
 
-export let webSocketUrl = `${protocol}://${host}/ws/`;
-export const requestPerSec = 10;     // req/sec
-export const canvasName = 'threeCanvas';
-export const lightColor = 'rgb(175,175,175)';
-export const darkColor = 'rgb(128,128,128)';
-export const minDate = new Date("2024-10-07T23:58:51.818Z"); // Minimum allowed date
-export const maxDate = new Date("2028-01-01T00:00:00.000Z"); // Maximum allowed date
+// ─────────────────────────────────────────────
+// Global Configuration
+// ─────────────────────────────────────────────
+
+export let webSocketUrl = `${protocol}://${host}/ws/`;              // WebSocket URL
+export const requestPerSec = 10;                                    // request/second
+
+export const darkColor = 'rgb(128,128,128)';                      // Color for buttons
+export const lightColor = 'rgb(175,175,175)';                     // Color for buttons when active
+
+export const minDate = new Date("2024-10-07T23:58:51.818Z");        // Minimum allowed date
+export const maxDate = new Date("2028-01-01T00:00:00.000Z");        // Maximum allowed date
+export const defaultDate = new Date("2025-03-12T09:27:00.000Z");    // Default date for simulation
