640205d6b15cbee05e4e5c3246debe6e438c3b12
drmendel <dobondireiszmendel@gmail.com>
2025-05-28 20:17:19 +0200

    Set main as entrypoint

--- src/config.js
+++ src/config.js
@@ -1,12 +1,14 @@
-// ###################### GLOBAL VARIABLES ######################
+// ─────────────────────────────────────────────
+// Configurational values
+// ─────────────────────────────────────────────
 
 const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
-const host = window.location.host; // e.g., mywebsite.com
+const host = window.location.host;                           // e.g., mywebsite.com
 
 export let webSocketUrl = `${protocol}://${host}/ws/`;
-export const requestPerSec = 10;     // req/sec
-export const canvasName = 'threeCanvas';
-export const lightColor = 'rgb(175,175,175)';
-export const darkColor = 'rgb(128,128,128)';
+export const requestPerSec = 10;                             // req/sec
+export const canvasName = 'threeCanvas';                     // three.js canvas
+export const lightColor = 'rgb(175,175,175)';              // light color for active buttons
+export const darkColor = 'rgb(128,128,128)';               // dark for not active buttons
 export const minDate = new Date("2024-10-07T23:58:51.818Z"); // Minimum allowed date
 export const maxDate = new Date("2028-01-01T00:00:00.000Z"); // Maximum allowed date
