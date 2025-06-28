8a31357f8da189e84dc4aa348ee33ad1151d0163
drmendel <dobondireiszmendel@gmail.com>
2025-06-28 23:26:24 +0200

    Update license

--- src/config.js
+++ src/config.js
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2025 Mendel Dobondi-Reisz
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 // ─────────────────────────────────────────────
 // Configurational values
 // ─────────────────────────────────────────────
@@ -5,7 +21,7 @@
 const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
 const host = window.location.host;                           // e.g., mywebsite.com
 
-export let webSocketUrl = `${protocol}://${host}/ws/`;
+export let webSocketUrl = "ws://localhost:8080/ws/"; //`${protocol}://${host}/ws/`;
 export const requestPerSec = 10;                             // req/sec
 export const canvasName = 'threeCanvas';                     // three.js canvas
 export const lightColor = 'rgb(175,175,175)';              // light color for active buttons
