8d5d2938a96ac9a6b0cbff4732d4a25ad0097233
Dobondi-Reisz Mendel <dobondireiszmendel@gmail.com>
2025-10-01 11:45:00 +0200

    Limit maxDate based on SPICE kernel coverage

--- src/config.js
+++ src/config.js
@@ -27,4 +27,4 @@ export const canvasName = 'threeCanvas';                     // three.js canvas
 export const lightColor = 'rgb(175,175,175)';              // light color for active buttons
 export const darkColor = 'rgb(128,128,128)';               // dark for not active buttons
 export const minDate = new Date("2024-10-07T23:58:51.818Z"); // Minimum allowed date
-export const maxDate = new Date("2028-01-01T00:00:00.000Z"); // Maximum allowed date
+export const maxDate = new Date("2027-02-01T00:00:00.000Z"); // Maximum allowed date
